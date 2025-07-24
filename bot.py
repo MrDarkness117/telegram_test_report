5a1252ba32797268d425c508971f009ede793442
Mikhail Romantsov <miromantsov@yandex.ru>
2025-07-24 16:10:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,3 @@
 from telebot import TeleBot  # telebot из библиотеки pyTelegramBotAPI
-bot = TeleBot("7597308621:AAGx4qHEDQNyQgVknRnYUk2UVeIeHuC-qp8", "HTML")
+bot = TeleBot("7597308621:AAGx4qHEDQNyQgVknRnYGHfw1eIeHuC-qp8", "HTML")
 print("Bot token acquired.")
